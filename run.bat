d5c54c1c12b7097522c8c719908273eb85ce8726
Peter Nguyen <nguyenph882@gmail.com>
2018-02-06 11:20:37 -0800

    added the wrong file

--- run.bat
+++ run.bat
@@ -2,6 +2,7 @@
 setlocal enabledelayedexpansion
 
 if exist output.txt del output.txt
+echo class   Category_666 {> output.txt
 set "temp="
 for /r %%i in (*.hpp) do (
   if "%%~nxi" NEQ "output.txt" (
@@ -10,3 +11,4 @@ for /r %%i in (*.hpp) do (
     if "!temp!" NEQ "" Echo !temp!
     set temp=%%j
 ))) >> output.txt
+echo };>> output.txt
